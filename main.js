381887159a3b88bd9fa28d7b2a838e73dc57f905
Hapachino <erickwbrownfield@gmail.com>
2018-11-07 10:28:28 -0800

    Memory Match version0.5 - Erick Brownfield

--- main.js
+++ main.js
@@ -1,14 +1,70 @@
-const cards = 18;
+const config = {
+  cards: 4,
+  totalPossibleMatches() {
+    return this.cards / 2;
+  }
+}
 
-$('document').ready(createCards.bind(this, cards));
+const gameState = {
+  firstCardClicked: null,
+  secondCardClicked: null,
+  clicked: 0,
+  matchCounter: 0,
+};
+
+$('document').ready(function() {
+  createCards(config.cards);
+  $('.card').click(cardClicked);
+});
 
 function createCards(number) {
   for (let i = 0; i < number; i++) {
     const card = $('<div>').addClass('card');
-    const front = $('<div>').addClass('front');
+    const front = $('<div>').addClass('front').text(i / 2 | 0);
     const back = $('<div>').addClass('back');
 
     card.append(front, back);
     $('main').append(card);
   }
+}
+
+function cardClicked() {
+  if (gameState.clicked === 2) {
+    return;
+  } else {
+    gameState.clicked++;
+  }
+
+  $(this).find('.back').addClass('hidden');
+
+  if (!gameState.firstCardClicked) {
+    gameState.firstCardClicked = $(this);
+  } else {
+    gameState.secondCardClicked = $(this);
+
+    if (gameState.firstCardClicked.text() === gameState.secondCardClicked.text()) {
+      delayCardInteraction('matched');
+      gameState.matchCounter++;
+
+      if (gameState.matchCounter === config.totalPossibleMatches()) {
+        console.log('You won!');
+      } 
+    } else {
+      delayCardInteraction('mismatched');
+    }
+  }
+}
+
+function delayCardInteraction(isMatched) {
+  const ms = isMatched === 'mismatched' ? 2000 : 0;
+
+  setTimeout(function() {
+    if (isMatched === 'mismatched') {
+      gameState.firstCardClicked.find('.back').removeClass('hidden');
+      gameState.secondCardClicked.find('.back').removeClass('hidden');
+    }
+
+    gameState.firstCardClicked = gameState.secondCardClicked = null;
+    gameState.clicked = 0;
+  }, ms);
 }
