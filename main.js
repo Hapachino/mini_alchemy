fdf8cf85e5b03298b1bab6aa384bba4899ac6894
Hapachino <erickwbrownfield@gmail.com>
2018-11-08 21:55:39 -0800

    revised to alchemy style game

--- main.js
+++ main.js
@@ -1,43 +1,95 @@
 $('document').ready(function () {
-  createCards(config.cards);
-  $('.card').click(cardClicked);
+  createStartingCards(config.startingCards, '#starting-cards-area');
+  $('#game-area').on('click', '.card', cardClicked);
   $('.reset').click(reset);
 });
 
 const config = {
-  cards: 18,
-  totalPossibleMatches() {
-    return this.cards / 2;
-  },
+  startingCards: ['air', 'fire', 'earth', 'water'],
+  cardBack: 'transmutation',
   flipDelay: 1000,
+  formulas: {
+    // tier 1
+    // air, fire, earth, water
+
+    // tier 2
+    'air + air': 'pressure',
+    'air + fire': 'smoke',
+    'air + earth': 'dust',
+    'air + water': 'mist',
+    'earth + earth': 'land',
+    'earth + water': 'mud',
+    'earth + fire': 'lava',
+    'fire + fire': 'energy',
+    'fire + water': 'steam',
+    'water + water': 'puddle',
+
+    // tier 3
+    'air + energy': 'heat',
+    'air + metal': 'rust',
+    'air + pressure': 'wind',
+    'earth + pressure': 'stone',
+    'dust + fire': 'gunpowder',
+    'earth + lava': 'volcano',
+    'lava + water': 'obsidian',
+
+    // tier 4
+    'air + metal': 'rust',
+    'fire + stone': 'metal',
+    'fire + gunpowder': 'explosion',
+    'stone + stone': 'wall',
+
+    // tier 5
+    'wall + wall': 'house',
+    'energy + explosion': 'atomic_bomb',
+  },
 }
 
 const gameState = {
   firstCardClicked: null,
   secondCardClicked: null,
   clicked: 0,
-  matchCounter: 0,
 };
 
 const gameStats = {
   matches: 0,
   attempts: 0,
-  accuracy: 0,
-  gamesPlayed: 0
+  gamesPlayed: 0,
 }
 
-function createCards(number) {
-  for (let i = 0; i < number; i++) {
-    const card = $('<div>').addClass('card');
-    const front = $('<div>').addClass('front').text(i / 2 | 0);
-    const back = $('<div>').addClass('back');
+function createCard(element, parent) {
+  const card = $('<div>').addClass('card').attr('name', element);
 
-    card.append(front, back);
-    $('main').append(card);
+  const front = $('<div>').addClass('front');
+  const frontImage = $('<div>', {
+    class: 'front-image',
+    css: {
+      backgroundImage: `url(images/${element}.png)`,
+    }
+  });
+  front.append(frontImage);
+
+  const back = $('<div>').addClass('back');
+  const backImage = $('<div>', {
+    class: 'back-image',
+    css: {
+      backgroundImage: `url(images/${config.cardBack}.png)`,
+    }
+  });
+  back.append(backImage);
+
+  card.append(front, back);
+  $(parent).append(card);
+}
+
+function createStartingCards(cards, parent) {
+  for (let i = 0; i < cards.length; i++) {
+    createCard(cards[i], parent);
   }
 }
 
 function cardClicked() {
+  // check if two cards have already been flipped
   if (gameState.clicked === 2) {
     return;
   } else {
@@ -51,51 +103,41 @@ function cardClicked() {
   } else {
     gameState.secondCardClicked = $(this);
 
-    if (gameState.firstCardClicked.text() === gameState.secondCardClicked.text()) {
-      delayCardInteraction('matched');
-      gameStats.matches++;
-
-      if (gameStats.matches === config.totalPossibleMatches()) {
-        console.log('You won!');
-      } 
+    const firstElement = gameState.firstCardClicked.attr('name');
+    const secondElement = gameState.secondCardClicked.attr('name');
+    const search = [firstElement, secondElement].sort().join(' + ');
+    const newElement = config.formulas[search];
+    
+    // if no such formula
+    if (!newElement) {
+      console.log('formula doesn\'t exist');
     } else {
-      delayCardInteraction('mismatched');
+      createCard(newElement, '#created-cards-area');
     }
 
     gameStats.attempts++;
+    flipToCardBack();
     displayStats();
   }
 }
 
-function delayCardInteraction(isMatched) {
-  const ms = isMatched === 'mismatched' ? config.flipDelay : 0;
-
+function flipToCardBack() {
   setTimeout(function() {
-    if (isMatched === 'mismatched') {
-      gameState.firstCardClicked.find('.back').removeClass('hidden');
-      gameState.secondCardClicked.find('.back').removeClass('hidden');
-    }
+    gameState.firstCardClicked.find('.back').removeClass('hidden');
+    gameState.secondCardClicked.find('.back').removeClass('hidden');
 
     gameState.firstCardClicked = gameState.secondCardClicked = null;
     gameState.clicked = 0;
-  }, ms);
+  }, config.flipDelay);
 }
 
 function displayStats() {
   $('.games-played .value').text(gameStats.gamesPlayed);
   $('.attempts .value').text(gameStats.attempts);
-
-  if (gameStats.attempts) {
-    gameStats.accuracy = Math.floor(gameStats.matches / gameStats.attempts * 100) + '%';
-  } else {
-    gameStats.accuracy = 'N/A';
-  }
-
-  $('.accuracy .value').text(gameStats.accuracy);
 }
 
 function resetStats() {
-  gameStats.accuracy = gameStats.attempts = gameStats.matches = 0;
+  gameStats.attempts = 0;
   displayStats();
 }
 
@@ -103,32 +145,18 @@ function reset() {
   gameStats.gamesPlayed++;
   resetStats();
   displayStats();
-  $('.back').removeClass('hidden');
 }
 
-const recipes = {
-  // tier 1
-  'uncooked rice': {},
-  'water': {},
-  'fire': {},
-  'knife': {},
-  // 'seaweed': {},
-  'salmon': {},
-  'tuna': {},
-  'vinegar mix': {},
-  // 'bamboo mat': {},
-  // tier 2
-  // tier 3
-  // tier 4
-}
-
-const formulas = {
-  // tier 1
-  air: {},
-  fire: {},
-  earth: {},
-  water: {},
-  // tier 2
-  // tier 3
-  // tier 4
-}
+/*
+TODO:
+new cards layout
+show new element before flipping back
+flip card animation
+show goal card
+combine card animation
+combine card sound
+settings 
+about
+story line
+intro
+*/
