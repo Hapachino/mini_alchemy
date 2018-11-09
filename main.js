1fbaec3cbdf4f8f6113490df17cc8b35bf5fefa9
Hapachino <erickwbrownfield@gmail.com>
2018-11-08 17:42:18 -0800

    Memory Match version1.0 - Erick Brownfield

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 $('document').ready(function () {
   createCards(config.cards);
   $('.card').click(cardClicked);
+  $('.reset').click(reset);
 });
 
 const config = {
   cards: 18,
   totalPossibleMatches() {
     return this.cards / 2;
-  }
+  },
+  flipDelay: 1000,
 }
 
 const gameState = {
@@ -16,6 +18,13 @@ const gameState = {
   clicked: 0,
   matchCounter: 0,
 };
+
+const gameStats = {
+  matches: 0,
+  attempts: 0,
+  accuracy: 0,
+  gamesPlayed: 0
+}
 
 function createCards(number) {
   for (let i = 0; i < number; i++) {
@@ -44,19 +53,22 @@ function cardClicked() {
 
     if (gameState.firstCardClicked.text() === gameState.secondCardClicked.text()) {
       delayCardInteraction('matched');
-      gameState.matchCounter++;
+      gameStats.matches++;
 
-      if (gameState.matchCounter === config.totalPossibleMatches()) {
+      if (gameStats.matches === config.totalPossibleMatches()) {
         console.log('You won!');
       } 
     } else {
       delayCardInteraction('mismatched');
     }
+
+    gameStats.attempts++;
+    displayStats();
   }
 }
 
 function delayCardInteraction(isMatched) {
-  const ms = isMatched === 'mismatched' ? 2000 : 0;
+  const ms = isMatched === 'mismatched' ? config.flipDelay : 0;
 
   setTimeout(function() {
     if (isMatched === 'mismatched') {
@@ -67,4 +79,56 @@ function delayCardInteraction(isMatched) {
     gameState.firstCardClicked = gameState.secondCardClicked = null;
     gameState.clicked = 0;
   }, ms);
+}
+
+function displayStats() {
+  $('.games-played .value').text(gameStats.gamesPlayed);
+  $('.attempts .value').text(gameStats.attempts);
+
+  if (gameStats.attempts) {
+    gameStats.accuracy = Math.floor(gameStats.matches / gameStats.attempts * 100) + '%';
+  } else {
+    gameStats.accuracy = 'N/A';
+  }
+
+  $('.accuracy .value').text(gameStats.accuracy);
+}
+
+function resetStats() {
+  gameStats.accuracy = gameStats.attempts = gameStats.matches = 0;
+  displayStats();
+}
+
+function reset() {
+  gameStats.gamesPlayed++;
+  resetStats();
+  displayStats();
+  $('.back').removeClass('hidden');
+}
+
+const recipes = {
+  // tier 1
+  'uncooked rice': {},
+  'water': {},
+  'fire': {},
+  'knife': {},
+  // 'seaweed': {},
+  'salmon': {},
+  'tuna': {},
+  'vinegar mix': {},
+  // 'bamboo mat': {},
+  // tier 2
+  // tier 3
+  // tier 4
+}
+
+const formulas = {
+  // tier 1
+  air: {},
+  fire: {},
+  earth: {},
+  water: {},
+  // tier 2
+  // tier 3
+  // tier 4
 }
